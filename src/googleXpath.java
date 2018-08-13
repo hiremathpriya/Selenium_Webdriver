18d5517ed9ede779d1e5c35714815b7a0e50c8a2
Priya Hiremath <priyahrish18@gmail.com>
2018-08-13 13:04:50 +1200

    added chropath plugin to find the xpath from browser

--- src/googleXpath.java
+++ src/googleXpath.java
@@ -10,8 +10,12 @@ public class googleXpath {
 		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 		WebDriver driver= new ChromeDriver();
 		driver.get("http://google.com");//hit url on browser
-		driver.findElement(By.xpath("//div[@class='lst-c']/div/div[2]/div/input")).sendKeys("India");
+		//xpath exapmle by using parent child relationship
+		//driver.findElement(By.xpath("//div[@class='lst-c']/div/div[2]/div/input")).sendKeys("India");
+		driver.findElement(By.xpath("//input[@id='lst-ib']")).sendKeys("Hi");
 
+		
+		
 	}
 
 }
